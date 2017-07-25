b9d136d76d4514e3f2a17bb18cf3417b5b1a78f5
neebulo <rhapsodyshadow@yahoo.com>
2017-07-25 23:15:50 +0000

    rspec up to hypehnize

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in person.gemspec
 gemspec
+
+gem 'activesupport', '~> 5.0'
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,44 @@
 require 'homework05/version'
+require 'active_support/core_ext/string' 
+# requires are used if a gemfile need to be used
 
 class Person
-  # add your implementation here
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name 
+    @history = [@name]
+  end
+
+  def to_s
+    @history.last
+  end
+
+  def upcase
+    @history << @name.upcase
+  end
+
+  def downcase
+    @history << @name.downcase
+  end
+
+  def titlelize
+    @history << @name.capitalize
+  end
+
+  def reverse
+    @history << @name.reverse
+  end
+
+  def hyphenize
+    @history << @name.chars.join("-")
+  end
+
+  def undo
+    @history << @name.previous_changes
+  end
+
+
+
+
 end
